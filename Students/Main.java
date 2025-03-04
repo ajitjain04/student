109ce8d2b4a5689d4a38ea817c3aa71484b354a1
Ajit Jain <jainajit787@gmail.com>
2025-03-04 22:13:26 +0530

    Do-while loop

--- Students/Main.java
+++ Students/Main.java
@@ -8,6 +8,35 @@ class Main{
 		int choice = 1 ;
 		StudentOperations operations = new StudentOperations();
 
+		do{//mUltiple students
+		System.out.println("Enter Name:");
+		String name = scan.nextLine();
+
+		System.out.println("Enter PRN:");
+		int prn = Integer.parseInt(scan.nextLine());
+
+		System.out.println("Enter Branch:");
+                String branch = scan.nextLine();
+
+            	System.out.println("Enter Batch:");
+            	String batch = scan.nextLine();
+
+            	System.out.println("Enter CGPA:");
+            	float cgpa = Float.parseFloat(scan.nextLine());
+
+
+
+		Student student = new Student(name ,prn ,branch, batch, cgpa);
+		operations.addStudent(student);//Adding student
+
+		System.out.println("Enter choice 1.Continue 2.Exit:");
+		choice = Integer.parseInt(scan.nextLine());
+		}while(choice !=2);
+
+		operations.displayStudents();//Display students
+
+		
+
 	}
 
 }
