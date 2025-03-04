cadad4731454d9de2391321f81394a3e3b093fde
Ajit Jain <jainajit787@gmail.com>
2025-03-04 22:13:12 +0530

    Object creation

--- Students/Main.java
+++ Students/Main.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 class Main{
 	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int choice = 1 ;
+		StudentOperations operations = new StudentOperations();
+
 	}
 
 }
